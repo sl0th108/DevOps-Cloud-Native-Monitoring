529bafbd90970d97d6450a45641b3cb9b403631a
Sl0th108 <bcondelario@gmail.com>
2025-09-24 18:20:20 -0600

    spelling errors fixed

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-test
+import psutil 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    cpu_percent = psutil.cpu.percent()
+    mem_percent = psutil.virtual_memory().percent
+    message = None
+    if cpu_percent > 80 or mem_percent > 80:
+        Message = "High CPU or Memory Usage Detected, Please Scale Up."
+    return f"CPU Utilization: {cpu_percent} and Memory Utilization: {mem_percent}"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
